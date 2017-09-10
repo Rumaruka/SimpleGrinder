a5491e4b92d0b0bb7d57a3d92ac702f0d7b6b517
bence98 <bence.csokas@gmail.com>
2017-09-10 13:08:02 +0200

    Removed ExampleMod, added OreDictionary for flour

--- java/com/rumaruka/simplegrinder/Common/OreDict/OreDict.java
+++ java/com/rumaruka/simplegrinder/Common/OreDict/OreDict.java
@@ -8,16 +8,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class OreDict {
-	
-	
-	
 
 	public static void registerOre(){
 	 OreDictionary.registerOre("dustIron", new ItemStack(ItemsCore.dust_iron));
-	 OreDictionary.registerOre("dustGold",new ItemStack(ItemsCore.dust_gold));
+	 OreDictionary.registerOre("dustGold", new ItemStack(ItemsCore.dust_gold));
+	 OreDictionary.registerOre("dustWheat",new ItemStack(ItemsCore.flour));
 	}
-	
-	
-
 
 }
